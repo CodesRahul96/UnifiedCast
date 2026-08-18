a39b3491c1ba87e2164d8962ffdf86928008930b
codesrahul96 <codesrahul96@gmail.com>
2026-08-18 14:11:57 +0530

    Fix: Add missing BorderStroke import for Card borders

--- android/app/src/main/java/com/codesrahul/unifiedcast/MainActivity.kt
+++ android/app/src/main/java/com/codesrahul/unifiedcast/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.*
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.ui.input.pointer.pointerInput
